1f4512cac7cebdc46776426f22cc520b585ba6c1
hqying <hqying.z@gmail.com>
2018-09-12 14:09:27 +0800

    add http io

---
+++ src/pages/demo/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <ul class="container log-list">
+      <li v-for="(log, index) in logs" :key="index" class="log-item">
+        <card :text="log.id + ' . ' + log.city"></card>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {http} from '@/service/base'
+import card from '@/components/card'
+
+export default {
+  components: {
+    card
+  },
+
+  data () {
+    return {
+      logs: []
+    }
+  },
+
+  onLoad() {
+    let self = this;
+    http.get('https://www.4slx.com/index/index/foursstoreinfo', {})
+      .then( function(res) {self.logs = res; console.log(self.logs)})
+      .catch( function() {})
+  }
+}
+</script>
+
+<style>
+.log-list {
+  display: flex;
+  flex-direction: column;
+  padding: 40rpx;
+}
+
+.log-item {
+  margin: 10rpx;
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -19,6 +19,7 @@
       <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
     </form>
     <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
+    <a href="/pages/demo/main" class="counter">去往demo页面</a>
   </div>
 </template>
 
